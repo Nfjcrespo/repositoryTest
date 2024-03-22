f35b5490a35543e41320d8bafc94772e36a0678b
Nfjcrespo <144074552+Nfjcrespo@users.noreply.github.com>
2024-03-22 13:24:20 +0100

    Add files via upload

---
+++ CrudGestionLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDGestionLivres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudGestionLivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
